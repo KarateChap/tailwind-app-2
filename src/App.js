e950de4c6a1cb46772c94ea602a9d64b8b7b9d2b
Ralph Gabriel Mariano <57673613+KarateChap@users.noreply.github.com>
2022-08-27 20:23:25 +0800

    added hero and navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./component/Hero";
 import Navbar from "./component/Navbar";
 import "./index.css";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
