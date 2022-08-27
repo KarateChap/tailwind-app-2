fb9b9bb8faea6995563a3e8c1f593ece0b79bb7b
Ralph Gabriel Mariano <57673613+KarateChap@users.noreply.github.com>
2022-08-27 09:38:43 +0800

    added logo and created navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
